98ce0d1747a3cf25292f8320a6939db946dfe56a
MikePio <michelepiopilla@gmail.com>
2023-04-25 19:36:37 +0200

    Generated an email address

--- script.js
+++ script.js
@@ -9,15 +9,39 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 const { createApp } = Vue;
 
-createApp({
+createApp ({
   data() {
     return {
-      title: 'test vue',
-      
+      mail: '',
+      error: false
     }
   },
 
   methods: {
+
+  },
+
+  computed: {
+    
+    
+  },
+
+  mounted() {
+    // richiesta
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    //! test errore
+    // axios.get('https://Fflynn.boolean.careers/exercises/api/random/mail')
+    // dati ottenuti
+    .then( (result) => {
+      console.log(result.data);
+      
+      this.mail = result.data.response
+    })
+    // errori ottenuti
+    .catch( error => {
+      console.log(error);
+      this.error = true
+    })
 
   }
 
